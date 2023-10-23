3d68a9f6a3ae865083c146b0884b3e39f01a6279
MargaritaRoda <MargaritaRoda1984@gmail.com>
2023-10-23 22:12:03 +0300

    Deleted unnecessary elements

--- src/components/TitlePageAbout/Container.jsx
+++ src/components/TitlePageAbout/Container.jsx
@@ -5,7 +5,6 @@ import classNames from 'classnames';
 
 import styles from './Container.module.scss';
 export const Container = ({ className, children }) => {
-  console.log('Hello world');
   return (
     <div className={classNames(styles.container, className)}>{children}</div>
   );
